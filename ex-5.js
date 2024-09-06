2b4494832a68d568a7284dacc7546c1b19ddeb0e
FrankCW <wongasa999@gmail.com>
2024-09-06 19:39:28 +0700

    Assignment Java JavaScript Primitive Values Assignment Ex.1-8

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,9 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let ShoppingPrice = 27000
+let Discount = ShoppingPrice * 0.25;
+console.log(Discount)
+
+let DiscountTotalPrice = ShoppingPrice - Discount
+console.log(DiscountTotalPrice)
